fe41c38faae650fc255ee85411faf29d43dd93b9
Mukund Khandelwal <145903279+mukundkhandelwal463@users.noreply.github.com>
2026-07-27 23:51:26 +0530

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.cpp
@@ -0,0 +1,36 @@
+class TimeMap {
+public:
+    unordered_map<string, vector<pair<int, string>>> mp;
+
+    TimeMap() {
+
+    }
+
+    void set(string key, string value, int timestamp) {
+        mp[key].push_back({timestamp, value});
+    }
+
+    string get(string key, int timestamp) {
+        if (mp.find(key) == mp.end())
+            return "";
+
+        vector<pair<int, string>>& v = mp[key];
+
+        int left = 0;
+        int right = v.size() - 1;
+        string ans = "";
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (v[mid].first <= timestamp) {
+                ans = v[mid].second;
+                left = mid + 1;     // search for a later valid timestamp
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
